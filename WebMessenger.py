7b255340c97d0854ddfe793bec43ff82fbe02197
Paolo <paolorosettani97@gmail.com>
2022-07-09 12:09:37 +0200

    message shorten

--- WebMessenger.py
+++ WebMessenger.py
@@ -18,7 +18,7 @@ print("1-Connect this PC to wi-fi (or ethernet).")
 print("2-Connect another device to the same network, wi-fi or ethernet (i.e. Smartphone).")
 print('3-Open this link on your external device\'s browser: ' + str(addr[0][4][0]) + ':2222')
 
-messaggio = 'INSTRUCTIONS:\n1-Connect this PC to wi-fi (or ethernet).\n2-Connect another device to the same network, wi-fi or ethernet (i.e. Smartphone). \n3-Open this link on your external device\'s browser:\n ' + str(addr[0][4][0]) + ':2222'
+messaggio = 'Connect to:\n ' + str(addr[0][4][0]) + ':2222'
 
 # Web interface:
 layout = [  [sg.Text('Live Messenger by Paolo Ros', size=(25,2))],
